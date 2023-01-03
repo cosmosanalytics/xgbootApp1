4d1e644c3b9b540cf133874af5b73a230dfdfde3
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-02 20:48:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,117 +1,272 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn
-
-import xgboost as xgb
-
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score, KFold
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import r2_score
-
+import plotly_express as px
 import streamlit as st
 
-header = st.container()
-dataset = st.container()
-Data_Preprocessing = st.container()
-model_training = st.container()
-model_plotten = st.container()
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import lightgbm as lgb
+from lightgbm import LGBMRegressor
+# from fbprophet import Prophet
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import GridSearchCV
 
 
-with header:
-    st.title("CO2 Emissions USA")
-    st.text("Dataset We will be using a dataset that encapsulates the carbon dioxide emissions generated from burning coal\n for producing electricity power in the United States of America between 1973 and 2016."
-            )
+
+# Read data from csv.
+data = pd.read_csv('/Users/skaraderili/PycharmProjects/anomaly_w_streamlit/streamlit_data.csv', sep=',')
 
 
-with dataset:
-    st.header("CO2 Emissions USA-Dataset")
-    st.text("I found this Dataset on Kaggle.com\n")
-    st.subheader("Dataset-Head:")
-    data = pd.read_csv("co2.csv")
-    st.write(data.head())
-
-    st.subheader("Dataset Description:")
-    st.write(data.describe())
-
-    st.subheader("CO2-Dataset-Shape:")
-    st.write(data.shape)
+# Change data type of date column. ( object to datetime)
+data['Date'] = pd.to_datetime(data["Date"], format='%Y-%m-%d')
+# Sort data by date column.
+data.sort_values(by=['Date'], inplace=True)
 
 
-with Data_Preprocessing:
-    st.header("This is the data after preprocessing")
-    data["Month"] = data.YYYYMM.astype(str).str[4:6].astype(float)
-    data["Year"] = data.YYYYMM.astype(str).str[0:6].astype(float)
-    data.drop(["YYYYMM"], axis=1, inplace=True)
-    data.replace([np.inf, -np.inf], np.nan, inplace=True)
-    st.subheader("Dataset-Head:")
-    st.write(data.head())
-    st.subheader("Dataset-Tail:")
-    st.write(data.tail())
+# Create new time columns.
+data['day_of_week'] = data['Date'].dt.dayofweek
+data['day_of_month'] = data['Date'].dt.day
+data['month'] = data['Date'].dt.month
+data['week_of_year'] = data['Date'].dt.week
+data['season'] = (data['Date'].dt.month % 12 + 3) // 3
 
-X=data.loc[:,["Month", "Year"]].values
-y=data.loc[:,"Value"].values
-data_dmatrix = xgb.DMatrix(X,label=y)
-print(data_dmatrix)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
+# Encode col1, col2, col3 variables.
+le = preprocessing.LabelEncoder()
+data['col1'] = le.fit_transform(data['col1'])
+data['col2'] = le.fit_transform(data['col2'])
+data['col3'] = le.fit_transform(data['col3'])
+
+def report_metric(pred, test, model_name):
+    mae = mean_absolute_error(pred, test)
+    mse = mean_squared_error(pred, test)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(test, pred)
+
+    metric_data = {'Metric': ['MAE', 'RMSE', 'R2'], model_name: [mae, rmse, r2]}
+    metric_df = pd.DataFrame(metric_data)
+
+    return metric_df
 
 
-with model_training:
-    st.subheader("Time to train the model!")
-    st.text("Using XGBoost:")
-
-    n_estimators, learningRate, subsample, colsample_bytree, max_depth, gamma = st.columns(6)
-
-    estimator = n_estimators.slider("What should be the Estimator of the model?", min_value=1, max_value=1000, value=1000, step=10)
-
-    learning_rate = learningRate.slider("What should be the Learning Rate of the model?", min_value=0.01, max_value=100.0, value=0.08, step=0.01)
-
-    SubSample = subsample.slider("What should be the Sub Sample of the model?", min_value=0.01, max_value=100.0, value=0.75, step=0.01)
-
-    ColsampleBytree = colsample_bytree.slider("What should be the Col-sample Bytree of the model?", min_value=1, max_value=100, value=1, step=1)
-
-    MaxDepth = max_depth.slider("What should be the Max-Depth of the model?", min_value=1, max_value=100, value=7, step=1)
-
-    Gamma = gamma.slider("What should be the Max-Depth of the model?", min_value=1, max_value=100, value=0, step=1)
-
-    reg_mod = xgb.XGBRegressor(n_estimators=estimator,learning_rate=learning_rate,subsample=SubSample,colsample_bytree=ColsampleBytree,max_depth=MaxDepth,gamma=Gamma,)
-
-    reg_mod.fit(X_train, y_train)
-
-    scores = cross_val_score(reg_mod, X_train, y_train, cv=10)
-
-    st.subheader("RESULTS:")
-
-    st.write("Mean cross-validation score: %.2f" % scores.mean())
-    reg_mod.fit(X_train, y_train)
-
-    predictions = reg_mod.predict(X_test)
-
-    rmse = np.sqrt(mean_squared_error(y_test, predictions))
-
-    st.write("RMSE: %f" % (rmse))
-
-    r2 = np.sqrt(r2_score(y_test, predictions))
-    st.write("R_Squared Score : %f" % (r2))
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-with model_plotten:
-    st.subheader("Time to plott the model!")
-
-    plt.figure(figsize=(10, 5), dpi=80)
-    x_ax = range(len(y_test))
-    plt.plot(x_ax, y_test, label="test")
-    plt.plot(x_ax, predictions, label="predicted")
-    plt.title("Carbon Dioxide Emissions - Test and Predicted data")
+def plot_preds(data_date,test_date, target, pred):
+    fig = plt.figure(figsize=(20,10))
+    plt.plot(data_date, target, label = 'Real')
+    plt.plot(test_date, pred, label = 'Pred')
     plt.legend()
-    st.pyplot(plt.show())
+    st.pyplot(fig)
 
-st.title("End......Thanks a lot")
+
+# Train test split
+
+test_period = -20 
+
+test = data[test_period:]
+train = data[:test_period]
+
+### Perpare for Model 1
+
+x_trainm1 = train[["col1", "col2", "col3", "day_of_week", "day_of_month", "month", "week_of_year", "season"]]
+y_trainm1 = train[["target"]]
+
+x_testm1 = test[["col1", "col2", "col3", "day_of_week", "day_of_month", "month", "week_of_year", "season"]]
+y_testm1 = test[["target"]]
+
+lr = LinearRegression()
+lr.fit(x_trainm1, y_trainm1)
+m1pred = lr.predict(x_testm1)
+
+
+metric1 = report_metric(m1pred, y_testm1, "Linear Regression")
+
+
+### Prepare for model 2
+
+x_trainm2 = train[["col1", "col2", "col3", "day_of_week", "day_of_month", "month", "week_of_year", "season"]]
+y_trainm2 = train[["target"]]
+
+x_testm2 = test[["col1", "col2", "col3", "day_of_week", "day_of_month", "month", "week_of_year", "season"]]
+y_testm2 = test[["target"]]
+
+xgb = XGBRegressor(n_estimators=1000, learning_rate=0.05)
+
+# Fit the model
+xgb.fit(x_trainm2, y_trainm2)
+
+# Get predictions
+m2pred = xgb.predict(x_testm2)
+
+
+metric2 = report_metric(m2pred, y_testm2, "XGB Regression")
+
+
+### Prepare for model 3 
+
+
+# We will use important columns.
+x_trainm3 = train[["col1", "col3", "day_of_week", "day_of_month"]]
+y_trainm3 = train[["target"]]
+
+x_testm3 = test[["col1", "col3", "day_of_week", "day_of_month"]]
+y_testm3 = test[["target"]]
+
+# fit scaler on training data
+norm = MinMaxScaler().fit(x_trainm3)
+# transform training data
+x_train_normm3 = pd.DataFrame(norm.transform(x_trainm3))
+# transform testing data
+x_test_normm3 = pd.DataFrame(norm.transform(x_testm3))
+
+# We tuned parameters below with best params.
+lgb_tune = LGBMRegressor(learning_rate=0.1,
+                                max_depth=2,
+                                min_child_samples=25,
+                                n_estimators=100,
+                                num_leaves=31)
+
+lgb_tune.fit(x_train_normm3, y_trainm3)
+
+m3pred = lgb_tune.predict(x_test_normm3)
+
+metric3 = report_metric(m3pred, y_testm3, "LGBM Regression")
+
+
+# Create a page dropdown
+page = st.sidebar.selectbox("""
+Hello there! I'll guide you!
+
+Please select model""", ["Main Page", "Linear Regressor", "XGB Regressor", "LGBM Regressor", "Compare Models"])
+
+#-------------------------------------------------
+
+
+if page == "Main Page":
+
+    ### INFO
+    st.title("Hello, welcome to sales predictor!")
+    st.write("""
+
+    This application predicts sales for the next 10 days with 3 different models
+    
+    # Sales drivers used in prediction: 
+    
+    - Date: date format time feature
+    - col1: categorical feature 
+    - col2: second categorical feature
+    - col3: third categorical feature
+    - target: target variable to be predicted
+      
+    """)
+
+
+    st.write("Lets plot sales data!")
+    st.line_chart(data[["Date", "target"]].set_index("Date"))
+
+#-------------------------------------------------
+
+elif page == "Linear Regressor":
+
+    # Base model, it uses linear regression.
+
+    st.title("Model 1: ")
+    st.write("Model 1 works with linear regression as base model.")
+    st.write("The columns it used are: col1, col2, col3, day_of_week, day_of_month, month, week_of_year, season")
+
+    st.write(metric1)
+
+
+    """
+    ### Real vs Pred. Plot for 1. Model
+    """
+
+    plot_preds(data["Date"],test["Date"], data["target"], m1pred)
+
+
+#-------------------------------------------------
+
+elif page == "XGB Regressor":
+
+# Model 2
+
+    st.title("Model 2: ")
+    st.write("Model 2 works with XGB Regressor.")
+    st.write("The columns it used are: col1, col2, col3, day_of_week, day_of_month, month, week_of_year, season")
+
+
+    st.write(metric2)
+
+    """
+    ### Real vs Pred. Plot for 2. Model
+    """
+
+    plot_preds(data["Date"],test["Date"], data["target"], m2pred)
+
+#-------------------------------------------------
+
+elif page == "LGBM Regressor":
+
+    # Model 3
+    # This time we use importants features with lgbm model.
+    # Applied min max scaler, onalization and parameter tuning.
+
+    st.title("Model 3:")
+
+    st.write("Model 3 works with LGBM Regressor.")
+    st.write("The columns it used are: col1, col3, day_of_week, day_of_month")
+    st.write("We normalized and scaled data and we choose most important features.")
+    st.write()
+
+
+    st.write(metric3)
+
+    """
+    ### Real vs Pred. Plot for 3. Model
+    """
+
+    plot_preds(data["Date"],test["Date"], data["target"], m3pred)
+
+
+#-------------------------------------------------
+
+elif page == "Compare Models":
+
+    # Compare models.
+    st.title("Compare Models: ")
+
+    all_metrics = metric1.copy()
+    all_metrics["XGB Regression"] =  metric2["XGB Regression"].copy()
+    all_metrics["LGBM Regression"] = metric3["LGBM Regression"].copy()
+    st.write(all_metrics)
+
+
+    #--------------------------------------------------
+
+    # Best Model
+
+    st.title("Best Model /XGB Regressor: ")
+    st.write("Lets plot best models predictions in detail.")
+
+    # Plot best model results.
+    plot_preds(test["Date"],test["Date"], test["target"], m2pred)
+
+    # Shıw rowbase best result and real 
+    st.write("Best Model Predictions vs Real")
+    best_pred = pd.DataFrame(test[["target"]].copy())
+    best_pred["pred"] = m2pred
+    st.write(best_pred)
+
+
+
+link='Made by [Sengul Karaderili](https://github.com/Sk1613/)'
+st.markdown(link,unsafe_allow_html=True)
+st.write("💛")
+
+
+
