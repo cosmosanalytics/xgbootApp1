5f459795b5cb8e72cf667b11b13d8a947ca36662
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-09 22:49:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -96,6 +96,6 @@ plot_preds(data["Date"],data["Date"], data["target"], pred_xgb)
 
 st.write("Model 3 works with Prophet.")
 st.write(df1_p)
-plot_preds(df1["ds"],df1["y"], forecast["ds"], forecast["y"])
+plot_preds(df1["ds"],df1["y"], forecast["ds"], forecast["yhat"])
 
 
