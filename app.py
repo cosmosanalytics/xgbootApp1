35625ce9e133e0ec95080c603701bad0c11706cb
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-09 21:52:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import GridSearchCV
 
 data = pd.read_csv('NAMtotal_s.csv').rename(columns={'index':'Date','y':'target'})
+st.write(data)
 data['Date'] = pd.to_datetime(data["Date"])
+st.write(data)
 data.sort_values(by=['Date'], inplace=True)
 
 data['day_of_week'] = data['Date'].dt.dayofweek
@@ -27,7 +29,7 @@ data['day_of_month'] = data['Date'].dt.day
 data['month'] = data['Date'].dt.month
 data['week_of_year'] = data['Date'].dt.week
 data['season'] = (data['Date'].dt.month % 12 + 3) // 3
-st.write(data)
+
 
 def report_metric(pred, test, model_name):
     mae = mean_absolute_error(pred, test)
